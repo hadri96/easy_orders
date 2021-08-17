8e09ca59b67f515e45a134e53f04ac899d932428
Alexandre Stutz <alexandre.stutz@hotmail.com>
2021-08-17 16:37:41 +0200

    thats ok

--- db/migrate/20210817123334_add_is_confirmed_and_is_delivered_to_orders.rb
+++
@@ -1,6 +0,0 @@
-class AddIsConfirmedAndIsDeliveredToOrders < ActiveRecord::Migration[6.0]
-  def change
-    add_column :orders, :is_confirmed, :boolean, default: false
-    add_column :orders, :is_delivered, :boolean, default: false
-  end
-end
