672566bd5789398ae9be347a04944d054c232567
hadrien morand <hadrien.morand@ehl.ch>
2021-08-19 10:17:41 +0200

    Item create with Ingredient and IngredientList depencies done

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :items do
-    resources :orders, only: [:show]
-  end
+  resources :items
+  resources :orders, only: [:show]
   get '/orders/payment_method', to: 'orders#payment'
   get '/orders/payment_confirmation', to: 'orders#payment'
   get '/orders/thanks_message', to: 'orders#payment'
