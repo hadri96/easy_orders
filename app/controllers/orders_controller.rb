5adc259b8e54eeac3746f0277dd125969e1cf6c5
colin schmidt <colin.schm@gmail.com>
2021-08-19 15:53:42 +0200

    DB

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,7 @@
 class OrdersController < ApplicationController
   def show
-    @user = User.find(params[user_id])
-    @orders = @user.orders.select { |order| order.is_confirmed == false }
+    @order = Order.find(params[:id])
+    # @orders = @user.orders.select { |order| order.is_confirmed == false }
   end
   
   def payment_method
