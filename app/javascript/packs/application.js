419a1aea9ef0564582f96874e9c98cd388d85981
Alexandre Stutz <alexandre.stutz@hotmail.com>
2021-08-20 12:05:35 +0200

    push

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,9 +6,7 @@
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
-require("bootstrap")
 require("@nathanvda/cocoon")
-require("jquery")
 //= require cocoon
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   get '/orders/payment_method', to: 'orders#payment_method'
   get '/orders/payment_confirmation', to: 'orders#payment_confirmation'
   get '/orders/:id/thanks_message', to: 'orders#thanks_message'
-  get '/orders/:id/my_orders', to: 'orders#my_orders'
+  get '/orders/:id/my_orders', to: 'orders#my_orders', as: "my_orders"
+  get '/orders/:id/profile', to: 'orders#profile' 
 end
