79320db79ca5f7f06715c4d94e0da5b014d79f95
Alexandre Stutz <alexandre.stutz@hotmail.com>
2021-08-20 13:45:31 +0200

    finish

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   resources :orders, only: [:show]
   get '/orders/:id/payment_method', to: 'orders#payment_method'
   get '/orders/:id/thanks_message', to: 'orders#thanks_message'
-  get '/orders/:id/my_orders', to: 'orders#my_orders', as: "my_orders"
+  get '/account', to: 'orders#account', as: "account"
   get '/orders/:id/profile', to: 'orders#profile' 
+  get '/orders/:id/my_orders', to: 'orders#my_orders', as: "my_orders"
 end
