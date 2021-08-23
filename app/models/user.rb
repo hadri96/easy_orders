193d951c6721c620d4a7fbc61a7d4f2afb16b562
colin schmidt <colin.schm@gmail.com>
2021-08-23 17:17:37 +0200

    Navbar avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable
-  has_one_attached :photo
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i[facebook]
+
+  has_one_attached :avatar
 
 end
