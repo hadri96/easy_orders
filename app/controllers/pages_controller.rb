d415ef8f3fd23faafeb824e05ac337692d148322
colin schmidt <colin.schm@gmail.com>
2021-08-24 11:37:11 +0200

    problème de lien

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
   def home
-    @order = Order.find_by(user_id: current_user, is_paid: true, is_delivered: false)
+    @order = Order.find_by(user_id: current_user.id, is_paid: true, is_delivered: false)
   end
 end
