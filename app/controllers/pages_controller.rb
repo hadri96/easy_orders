29c2fe6fe6d8a6bc993ce496ed63e4b8afef07f7
Alexandre Stutz <alexandre.stutz@hotmail.com>
2021-08-24 11:53:28 +0200

    correctBug hey merge me!

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
   def home
-    @order = Order.find_by(user_id: current_user.id, is_paid: true, is_delivered: false)
+    @current_order = Order.find_by(user_id: current_user.id, is_paid: true, is_delivered: false)
   end
 end
