721dffd9676ca2ff92bb8840886c1e4f7d8b9f40
Alexandre Stutz <alexandre.stutz@hotmail.com>
2021-08-24 10:23:39 +0200

    betterView please merge me!

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,15 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable
-  has_one_attached :photo
+  
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i[facebook]
+  :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i[facebook]
+  has_one_attached :photo
+  has_many :orders
 
+  def has_on_going_order?
+    if self.orders.last.is_confirmed == true && self.orders.last.is_delivered == false
+      return true
+    end 
+  end
 end
