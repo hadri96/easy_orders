ff05822ed2d2fc9cad2c83ac6a6aaf5ccd7af2d0
Alexandre Stutz <alexandre.stutz@hotmail.com>
2021-08-26 11:33:18 +0200

    addDefaultValueAndwhiteList meeerge me

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,24 +1,19 @@
 class OrdersController < ApplicationController
 
-  before_action :find_ID, except: [:account, :my_archives]
+  before_action :set_order, except: [:account, :my_archives]
 
   def show
-    @order = Order.find(params[:id])
-    # @orders = @user.orders.select { |order| order.is_confirmed == false }
   end
 
   def payment_method
-    @order = Order.find(params[:id])
     @order.is_confirmed = true
     @order.save!
   end
 
   def payment_confirmation
-    @order = Order.find(params[:id])
   end
 
   def thanks_message
-    @order = Order.find(params[:id])
     @order.is_paid = true
     @order.save!
   end
@@ -30,12 +25,9 @@ class OrdersController < ApplicationController
   end
 
   def my_orders_notification
-    # @user = current_user.id
-    @order = Order.find(params[:id])
   end
 
   def my_orders
-    @order = Order.find(params[:id])
   end
 
   def my_archives
@@ -44,19 +36,26 @@ class OrdersController < ApplicationController
 
 
   def profile
-    @order = Order.find(params[:id])
   end
 
   def destroy
-    @order = Order.find(params[:id])
     @order.destroy
     redirect_to root_path
   end
 
+  def update
+    @order.update(order_params)
+    redirect_to order_path(@order)
+  end
+  
+
   private
 
-  def find_ID
+  def set_order
     @order = Order.find(params[:id])
   end
   
+  def order_params
+    params.require(:order).permit(:is_confirmed, :is_delivered, :user_id, :is_paid)
+  end  
 end
